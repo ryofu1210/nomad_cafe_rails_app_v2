674c39e70ab2d5285cdcaf65d1c77347b8c60743
ryofu1210 <inf.ryo@gmail.com>
2019-07-25 20:34:42 +0900

    cancancan ability設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActiveHash::RecordNotFound, with: :render_404
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = "権限がありません。"
+    redirect_to root_path
+  end
+
   def render_404(exception = nil)
     if exception
       logger.info "Rendering 404 with exception: #{exception.message}"
@@ -9,6 +15,11 @@ class ApplicationController < ActionController::Base
     # set_view_path
     render template: 'errors/error_404', status: 404
   end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_user)
+  end
+
 
 
 end
--- app/controllers/back/posts_controller.rb
+++ app/controllers/back/posts_controller.rb
@@ -1,4 +1,5 @@
 class Back::PostsController < ApplicationController
+  load_and_authorize_resource
   ITEMS_PER_PAGE = 3
 
   def index
